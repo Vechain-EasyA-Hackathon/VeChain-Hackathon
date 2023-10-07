21615351b0e4e41b1481c27597bbb1af31904201
Darsh Gondalia <51378290+DarshGondalia@users.noreply.github.com>
2023-10-07 19:53:20 -0400

    etherCall- connect()

--- src/etherCalls/connect.js
+++ src/etherCalls/connect.js
@@ -1,6 +1,12 @@
-var ethers = require('ethers');  
-var url = '127.0.0.1:8545';
-var customHttpProvider = new ethers.JsonRpcProvider(url);
-customHttpProvider.getBlockNumber().then((result) => {
-    console.log("Current block number: " + result);
-});
+import { Contract } from 'ethers';
+import { Seller_Product } from '../contracts/Seller_Product.json';
+// const [contract, setContract] = useState();
+export async function connect(){
+    var customHttpProvider = new ethers.JsonRpcProvider(url);
+    customHttpProvider.getBlockNumber().then((result) => {
+        console.log("Current block number: " + result);
+    });
+    
+    const seller = new Contract(tokenAddress, Seller_Product.abi, customHttpProvider);
+    console.log(seller);
+}
