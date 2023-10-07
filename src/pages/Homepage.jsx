5859e26cd25f7ee3e89500fb6b93629852350b63
Alex Zimmerman <alexzimmerman@Alexs-MacBook-Air.local>
2023-10-07 16:17:53 -0400

    MetaMask works

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+
+//MUI STUFF
 import Button from '@mui/material/Button';
 
+//METAMASK
+import { useSDK } from '@metamask/sdk-react';
+
 function Home() {
+
+  //MetaMask login stuff
+  const [account, setAccount] = React.useState(null);
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
+
+  const connect = async () => {
+    try {
+      console.log(`connecting...`);
+      const accounts = await sdk?.connect();
+      setAccount(accounts?.[0]);
+      console.log(`connected:`, accounts?.[0]);
+    } catch(err) {
+      console.warn(`failed to connect..`, err);
+    }
+  };
+
+
+
   return (
     <div>
 
@@ -11,7 +34,15 @@ function Home() {
           <h1 className="text-2xl font-bold ml-2 text-white">Green Clothing Company</h1>
         </div>
         <div className="flex items-center">
-          <Button variant="contained">Connect Wallet</Button>
+          <Button variant="contained" onClick={connect}>{connected && (
+        <div>
+          <>
+            {chainId && `Connected chain: ${chainId}`}
+            <p></p>
+            {account && `Connected account: ${account}`}
+          </>
+        </div>
+      )}</Button>
         </div>
       </div>
     
