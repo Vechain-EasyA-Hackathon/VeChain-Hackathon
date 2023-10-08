875515d563d4593701a0362efefe42f8d97af954
shankars99 <shankars0899@gmail.com>
2023-10-07 22:31:27 -0400

    process env works
    Signed-off-by: shankars99 <shankars0899@gmail.com>

--- src/etherCalls/connect.js
+++ src/etherCalls/connect.js
@@ -1,24 +1,15 @@
-import dotenv from 'dotenv';
 import { Contract } from 'ethers';
 import { ethers } from 'ethers';
 import { abi } from '../../out/Seller_Product.sol/Seller_Product.json';
-// dotenv.config();
 
-// const { SELLER_PRODUCT_ADDRESS } = process.env;
+const SELLER_PRODUCT_ADDRESS = import.meta.env.VITE_SELLER_PRODUCT_ADDRESS;
 
-// const [contract, setContract] = useState();
-export async function connect(){
-    alert('abi:' + abi);
-
-    const SELLER = 0x5FbDB2315678afecb367f032d93F642f64180aa3
-
-
+export async function connect() {
+    window.console.log("Connected to Frontend");
     const url = "http://localhost:8545";
     var customHttpProvider = new ethers.providers.JsonRpcProvider(url);
     customHttpProvider.getBlockNumber().then((result) => {
         window.console.log("Current block number: " + result);
     });
-    window.console.log("Connected to Frontend");
-    const seller = new Contract(SELLER, abi, customHttpProvider);
-    window.console.log('testing');
+    const seller = new Contract(SELLER_PRODUCT_ADDRESS, abi, customHttpProvider);
 }
