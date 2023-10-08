40298c0cd71f5248fd16039e579b9931d6d60027
Darsh Gondalia <51378290+DarshGondalia@users.noreply.github.com>
2023-10-07 20:16:48 -0400

    working connect()

--- src/etherCalls/connect.js
+++ src/etherCalls/connect.js
@@ -1,12 +1,18 @@
+import dotenv from 'dotenv';
 import { Contract } from 'ethers';
-import { Seller_Product } from '../contracts/Seller_Product.json';
+import { Seller_Product } from '../../out/Seller_Product.sol/Seller_Product.json';
+dotenv.config();
+
+const { SELLER_PRODUCT_ADDRESS } = process.env;
+
 // const [contract, setContract] = useState();
 export async function connect(){
+    const url = "http://localhost:8545";
     var customHttpProvider = new ethers.JsonRpcProvider(url);
     customHttpProvider.getBlockNumber().then((result) => {
         console.log("Current block number: " + result);
     });
     console.log("Connected to Frontend");
-    const seller = new Contract(tokenAddress, Seller_Product.abi, customHttpProvider);
-    console.log(seller);
+    const seller = new Contract(SELLER_PRODUCT_ADDRESS, Seller_Product.abi, customHttpProvider);
+    console.log(seller.address);
 }
