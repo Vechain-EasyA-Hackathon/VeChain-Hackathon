16ff5d7df927d4902d561ed5d4ce01344c428a95
Darsh Gondalia <51378290+DarshGondalia@users.noreply.github.com>
2023-10-07 23:14:48 -0400

    .

--- src/etherCalls/connect.js
+++ src/etherCalls/connect.js
@@ -1,23 +1,14 @@
 import { Contract, ethers } from 'ethers';
+import { abi } from '../../out/Seller_Product.sol/Seller_Product.json';
 
 const SELLER_PRODUCT_ADDRESS = import.meta.env.VITE_SELLER_PRODUCT_ADDRESS;
 
 export async function connect() {
-    window.console.log("Connected to Frontend");
+    window.console.log("");
     const url = "http://localhost:8545";
-    var customHttpProvider = new ethers.JsonRpcProvider(url);
-    window.console.log("Connected to Backend");
+    var customHttpProvider = new ethers.providers.JsonRpcProvider(url);
     customHttpProvider.getBlockNumber().then((result) => {
         window.console.log("Current block number: " + result);
-        window.console.log("Connected to Frontend");
-
     });
-    const seller = new Contract(SELLER_PRODUCT_ADDRESS, Seller_Product.abi, customHttpProvider);
-    seller.getProductCount().then((result) => {
-        window.console.log("count: " + result);
-    });
-    // await seller.addProduct(uint256(10), "Prod Name", "Today", uint256(100), "desc", "image", "category", uint256(25), "seller_name");
-    // const product = await seller.getProduct(uint256(10), "seller_name");
-    // console.log(product);
-    window.console.log(SELLER_PRODUCT_ADDRESS);
+    const seller = new Contract(SELLER_PRODUCT_ADDRESS, abi, customHttpProvider);
 }
