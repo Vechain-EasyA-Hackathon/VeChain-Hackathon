f61249408b8719cffdd8649842a629da309d2631
Alex Zimmerman <alexzimmerman@Alexs-MacBook-Air.local>
2023-10-07 22:08:19 -0400

    fuck the env

--- src/etherCalls/connect.js
+++ src/etherCalls/connect.js
@@ -1,18 +1,24 @@
 import dotenv from 'dotenv';
 import { Contract } from 'ethers';
-import { Seller_Product } from '../../out/Seller_Product.sol/Seller_Product.json';
-dotenv.config();
+import { ethers } from 'ethers';
+import { abi } from '../../out/Seller_Product.sol/Seller_Product.json';
+// dotenv.config();
 
-const { SELLER_PRODUCT_ADDRESS } = process.env;
+// const { SELLER_PRODUCT_ADDRESS } = process.env;
 
 // const [contract, setContract] = useState();
 export async function connect(){
+    alert('abi:' + abi);
+
+    const SELLER = 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+
     const url = "http://localhost:8545";
-    var customHttpProvider = new ethers.JsonRpcProvider(url);
+    var customHttpProvider = new ethers.providers.JsonRpcProvider(url);
     customHttpProvider.getBlockNumber().then((result) => {
-        console.log("Current block number: " + result);
+        window.console.log("Current block number: " + result);
     });
-    console.log("Connected to Frontend");
-    const seller = new Contract(SELLER_PRODUCT_ADDRESS, Seller_Product.abi, customHttpProvider);
-    console.log(seller.address);
+    window.console.log("Connected to Frontend");
+    const seller = new Contract(SELLER, abi, customHttpProvider);
+    window.console.log('testing');
 }
