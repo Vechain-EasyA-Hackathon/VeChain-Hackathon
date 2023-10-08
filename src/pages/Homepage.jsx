e4d400786673270b91c5fc4da6c65e5392c6439e
alavcode <126021458+alavcode@users.noreply.github.com>
2023-10-08 00:48:15 -0400

    added metamask

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,47 +1,64 @@
-import React from 'react'
-
-//MUI STUFF
+import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-
-//METAMASK
-import { useSDK } from '@metamask/sdk-react';
-
-//ethers
 import { ethers } from 'ethers';
-
-//Component imports
-import Metamask from '../MetaMaskFunctions/MetaMask';
-
-import { connect } from '../etherCalls/connect';
-
+import { addSeller } from '../etherCalls/addSeller';
 
 
 function Home() {
+  const [userAddress, setUserAddress] = useState(null);
+
+  useEffect(() => {
+    async function fetchAddress() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      try {
+        const accounts = await provider.listAccounts();
+        setUserAddress(accounts[0]); // Assuming the first account is the user's address
+      } catch (error) {
+        console.error('Error fetching address:', error);
+      }
+    }
+
+    fetchAddress();
+  }, []);
+
+  const handleDisconnect = async () => {
+    try {
+      await window.ethereum.request({ method: 'eth_disconnect' });
+      setUserAddress(null); // Disconnect by setting the address to null
+    } catch (error) {
+      console.error('Error disconnecting:', error);
+    }
+  };
+
+  const formattedAddress = userAddress ? `${userAddress.slice(0, 6)}...${userAddress.slice(-4)}` : '';
 
   return (
     <div>
-
       {/* Header */}
       <div className="flex justify-between items-center py-4 px-8 bg-emerald-500">
         <div className="flex items-center">
           <h1 className="text-2xl font-bold ml-2 text-white">Green Clothing Company</h1>
         </div>
         <div className="flex items-center">
-          
-          <Button variant="contained" onClick={connect}>Connect</Button>
-
+          {userAddress ? (
+            <div className="flex flex-col items-center ml-4">
+              <span className="text-sm mb-2">{formattedAddress}</span>
+              <Button variant="contained" onClick={handleDisconnect} className="bg-red-500">
+                Disconnect
+              </Button>
+            </div>
+          ) : (
+            <Button variant="contained" onClick={() => window.ethereum.request({ method: 'eth_requestAccounts' })}>
+              Connect
+            </Button>
+          )}
         </div>
+        <Button variant="contained" onClick={()=>addSeller("Nike", userAddress)} className="bg-red-500">
+                addSeller
+              </Button>
       </div>
-
-      
-
-    
-    
-      <Button variant="contained">Hello world</Button>
-    
     </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
