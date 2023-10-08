06b24312acd785100665345966a75e54a995ddae
alavcode <126021458+alavcode@users.noreply.github.com>
2023-10-07 21:44:03 -0400

    update to SellerDashboard and Mint

--- src/pages/Seller/Mint.jsx
+++ src/pages/Seller/Mint.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function Mint() {
+const MintPage = () => {
+  const parameters = ['Name', 'Product ID', 'Carbon Footprint', 'Quantity', 'Image', 'Data', 'Seller Name'];
+  const navigate = useNavigate();
+
+  const handleMint = () => {
+    // Handle minting logic
+    console.log('Minting NFT...');
+  };
+
+  const handleClick = () => {
+    navigate('/seller/dashboard');
+  };
+
   return (
-    <div>Mint</div>
-  )
-}
+    <div className="min-h-screen flex flex-col bg-gray-100 p-5">
+      <nav className="bg-blue-500 p-4 flex items-center">
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          Back to Dashboard
+        </Button>
+        <div className="flex-grow text-white text-center font-bold text-2xl">
+          Add a Product
+        </div>
+      </nav>
 
-export default Mint
+      <div className="max-w-lg mx-auto mt-5 p-5 border border-gray-300 rounded-lg text-center">
+        {parameters.map((param, index) => (
+          <TextField
+            key={index}
+            label={param}
+            fullWidth
+            variant="outlined"
+            margin="normal"
+            className="mb-3"
+          />
+        ))}
+      </div>
+
+      <div className="flex justify-center mt-3">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleMint}
+        >
+          Mint
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default MintPage;
