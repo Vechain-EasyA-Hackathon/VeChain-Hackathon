fe3570d1ff219c2588cd8e5dddaac4d544fec5fc
alavcode <126021458+alavcode@users.noreply.github.com>
2023-10-07 23:11:36 -0400

    connect fix

--- src/etherCalls/connect.js
+++ src/etherCalls/connect.js
@@ -5,11 +5,22 @@ import { abi } from '../../out/Seller_Product.sol/Seller_Product.json';
 const SELLER_PRODUCT_ADDRESS = import.meta.env.VITE_SELLER_PRODUCT_ADDRESS;
 
 export async function connect() {
-    window.console.log("Connected to Frontend");
-    const url = "http://localhost:8545";
-    var customHttpProvider = new ethers.providers.JsonRpcProvider(url);
-    customHttpProvider.getBlockNumber().then((result) => {
-        window.console.log("Current block number: " + result);
-    });
-    const seller = new Contract(SELLER_PRODUCT_ADDRESS, abi, customHttpProvider);
+    try {
+        console.log("Connecting to Frontend");
+
+        const url = "http://localhost:8545";
+        const customHttpProvider = new ethers.providers.JsonRpcProvider(url);
+
+        // Get the current block number
+        const currentBlockNumber = await customHttpProvider.getBlockNumber();
+        console.log("Current block number: " + currentBlockNumber);
+
+        const seller = new Contract(SELLER_PRODUCT_ADDRESS, abi, customHttpProvider);
+
+        console.log("Connected to Frontend");
+        return seller;
+    } catch (error) {
+        console.error("Error connecting to the Ethereum network:", error);
+        throw error;
+    }
 }
